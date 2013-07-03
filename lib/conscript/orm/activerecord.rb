b3496655eb0785c94959a502b522d808c4f30d21
Steve Lorek <steve@stevelorek.com>
2013-07-03 15:32:43 +0100

    Implemented scopes, save prevention callback

--- lib/conscript/orm/activerecord.rb
+++ lib/conscript/orm/activerecord.rb
@@ -3,7 +3,24 @@ require 'active_record'
 module Conscript
   module ActiveRecord
     def register_for_draft(options = {})
+      
       default_scope { where(is_draft: false) }
+
+      belongs_to :draft_parent, class_name: self
+      has_many :drafts, conditions: {is_draft: true}, class_name: self, foreign_key: :draft_parent_id, dependent: :destroy, inverse_of: :draft_parent
+      
+      before_save :check_no_drafts_exist
+
+      class_eval <<-RUBY
+        def self.drafts
+          where(is_draft: true)
+        end
+
+        private
+          def check_no_drafts_exist
+            drafts.count == 0
+          end
+      RUBY
     end
   end
 end
--- spec/conscript/orm/activerecord_spec.rb
+++ spec/conscript/orm/activerecord_spec.rb
@@ -5,8 +5,8 @@ class TestModel < ActiveRecord::Base; end
 class TestMigration < ActiveRecord::Migration
   def self.up
     create_table :test_model, :force => true do |t|
-    t.integer  "draft_parent_id"
-    t.boolean  "is_draft", :default => false
+      t.integer "draft_parent_id"
+      t.boolean "is_draft", :default => false
     end
   end
 
@@ -27,10 +27,56 @@ describe Conscript::ActiveRecord do
       TestModel.respond_to?(:register_for_draft).should == true
     end
 
-    it "creates the drafts scope" do
+    it "creates the default scope" do
       TestModel.should_receive(:default_scope).once
+      TestModel.register_for_draft
+    end
+
+    it "creates a belongs_to association" do
+      TestModel.should_receive(:belongs_to).once.with(:draft_parent, kind_of(Hash))
+      TestModel.register_for_draft
+    end
+
+    it "creates a has_many association" do
+      TestModel.should_receive(:has_many).once.with(:drafts, kind_of(Hash))
+      TestModel.register_for_draft
+    end
+
+    it "creates a before_save callback" do
+      TestModel.should_receive(:before_save).once.with(:check_no_drafts_exist)
       TestModel.register_for_draft
     end
   end
 
+  describe "#drafts" do
+    it "limits results to drafts" do
+      TestModel.should_receive(:where).once.with(is_draft: true)
+      TestModel.drafts
+    end
+  end
+
+  describe "#check_no_drafts_exist" do
+    before do
+      @subject = TestModel.new
+    end
+
+    context "when no drafts exist" do
+      before do
+        @subject.stub_chain(:drafts, :count).and_return(0)
+      end
+
+      it "returns true" do
+        @subject.send(:check_no_drafts_exist).should == true
+      end
+    end
+    context "when drafts exist" do
+      before do
+        @subject.stub_chain(:drafts, :count).and_return(1)
+      end
+      
+      it "returns false" do
+        @subject.send(:check_no_drafts_exist).should == false
+      end
+    end
+  end
 end
